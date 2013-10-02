524bac11c148dc08bd1b424265ee2cd4da474786
Christian Autermann <christian@autermann.org>
2013-10-02 19:14:59 +0200

    refactored client

---
+++ src/main/java/org/uncertweb/matlab/MLClient.java
@@ -0,0 +1,158 @@
+package org.uncertweb.matlab;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.CharStreams;
+import com.google.common.io.InputSupplier;
+import com.google.common.io.OutputSupplier;
+
+/**
+ * TODO JavaDoc
+ *
+ * @author Christian Autermann <c.autermann@52north.org>
+ */
+public class MLClient {
+    private static final Logger log = LoggerFactory.getLogger(MLClient.class);
+    private static final int CONNECT_TIMEOUT = 10 * 1000; // 10s to connect
+    private final MLHandler handler = new MLHandler();
+
+    /**
+     * Sends a request to a MATLAB server. The server must be using the supplied
+     * server.m and waiting for
+     * connections on the specified port.
+     *
+     * @param host    the address of the MATLAB server
+     * @param port    the port of the MATLAB server
+     * @param request the <code>MLRequest</code> to send
+     *
+     * @return the <code>MLResult</code> of the function
+     *
+     * @throws MLException if MATLAB encountered an error during function
+     *                     execution
+     * @throws IOException if the connection to the MATLAB server failed
+     */
+    // TODO: sometimes a SocketException gets thrown (possible to do with lots of requests in a short amount of time), for now there's just three attempts if this happens
+    public MLResult sendRequest(String host, int port, MLRequest request) throws
+            MLException, IOException {
+        SocketException thrown = null;
+        for (int attempt = 0; attempt < 3; attempt++) {
+            try {
+                return _sendRequest(connect(host, port), request);
+            } catch (SocketException e) {
+                thrown = e;
+            }
+        }
+        throw thrown;
+    }
+
+    public MLResult sendRequest(String mlProxyURL, MLRequest request) throws
+            MLException, IOException {
+        Connection connection = connect(mlProxyURL);
+        connection.getOutput().write("request=".getBytes());
+        return _sendRequest(connection, request);
+    }
+
+    private Connection connect(String host, int port) throws IOException {
+        final Socket socket = new Socket();
+        socket.connect(new InetSocketAddress(host, port), CONNECT_TIMEOUT);
+        return new Connection() {
+            @Override
+            public InputStream getInput() throws IOException {
+                return socket.getInputStream();
+            }
+
+            @Override
+            public OutputStream getOutput() throws IOException {
+                return socket.getOutputStream();
+            }
+
+            @Override
+            public void close() {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    log.error("Error closing socket", e);
+                }
+            }
+        };
+    }
+
+    private Connection connect(String mlProxyURL) throws IOException {
+        final URLConnection connection = new URL(mlProxyURL).openConnection();
+        connection.setDoOutput(true);
+        connection.setDoInput(true);
+        return new Connection() {
+            @Override
+            public InputStream getInput() throws IOException {
+                return connection.getInputStream();
+            }
+
+            @Override
+            public OutputStream getOutput() throws IOException {
+                return connection.getOutputStream();
+            }
+        };
+    }
+
+    private MLResult _sendRequest(Connection con, MLRequest request)
+            throws MLException, IOException {
+        try {
+            handler.outputRequest(request, con.getOutput());
+            return parseResponse(con);
+        } finally {
+            con.close();
+        }
+    }
+
+    private MLResult parseResponse(InputSupplier<? extends InputStream> in)
+            throws IOException, MLException {
+        String response = CharStreams.toString(CharStreams
+                .newReaderSupplier(in, Charsets.UTF_8));
+
+//        try {
+//            throw handler.parse(response, MLException.class);
+//        } catch (JsonParseException e1) {
+//            try {
+//                throw handler.parse(response, IOException.class);
+//            } catch (JsonParseException e2) {
+//                return handler.parse(response, MLResult.class);
+//            }
+//        }
+        // parse
+        // try for exception
+        // FIXME speedup, not the best
+        if (response.startsWith("{\"exception\"")) {
+            throw handler.parseException(response);
+        } else {
+            // not an error, must be result
+            return handler.parseResult(response);
+        }
+    }
+
+    private abstract class Connection implements InputSupplier<InputStream>,
+                                                 OutputSupplier<OutputStream> {
+        public void close() {
+            try {
+                getInput().close();
+            } catch (IOException e) {
+                log.error("Error closing input stream", e);
+            }
+            try {
+                getOutput().close();
+            } catch (IOException e) {
+                log.error("Error closing output stream", e);
+            }
+        }
+    }
+}
--- src/main/java/org/uncertweb/matlab/MLHandler.java
+++ src/main/java/org/uncertweb/matlab/MLHandler.java
@@ -1,18 +1,10 @@
 package org.uncertweb.matlab;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.net.InetSocketAddress;
-import java.net.Socket;
-import java.net.SocketAddress;
-import java.net.SocketException;
-import java.net.URL;
-import java.net.URLConnection;
 
 import org.uncertweb.matlab.json.IOExceptionDeserializer;
 import org.uncertweb.matlab.json.MLExceptionDeserializer;
@@ -24,190 +16,148 @@ import org.uncertweb.matlab.value.MLValue;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonParseException;
 
 /**
- * Handles MATLAB requests/responses. See {@link #sendRequest(String, int, MLRequest)} for how to send 
+ * Handles MATLAB requests/responses. See
+ * {@link #sendRequest(String, int, MLRequest)} for how to send
  * a request to a MATLAB server.
- * 
+ *
  * @author Richard Jones
  *
  */
 public class MLHandler {
+    private final Gson gson;
 
-	private Gson gson;
+    /**
+     * Creates a new
+     * <code>MLHandler</code> instance.
+     *
+     */
+    public MLHandler() {
+        GsonBuilder builder = new GsonBuilder();
+        builder.serializeSpecialFloatingPointValues();
+        builder.registerTypeAdapter(MLException.class,
+                                    new MLExceptionDeserializer());
+        builder.registerTypeAdapter(MLException.class,
+                                    new MLExceptionSerializer());
+        builder.registerTypeAdapter(IOException.class,
+                                    new IOExceptionDeserializer());
+        builder.registerTypeAdapter(MLRequest.class,
+                                    new MLRequestDeserializer());
+        builder.registerTypeAdapter(MLResult.class,
+                                    new MLResultDeserializer());
+        builder.registerTypeAdapter(MLValue.class,
+                                    new MLValueSerializer());
+        gson = builder.create();
+    }
 
-	/**
-	 * Creates a new <code>MLHandler</code> instance.
-	 * 
-	 */
-	public MLHandler() {
-		GsonBuilder builder = new GsonBuilder();
-		builder.serializeSpecialFloatingPointValues();
-		builder.registerTypeAdapter(MLException.class, new MLExceptionDeserializer());
-		builder.registerTypeAdapter(MLException.class, new MLExceptionSerializer());
-		builder.registerTypeAdapter(IOException.class, new IOExceptionDeserializer());
-		builder.registerTypeAdapter(MLRequest.class, new MLRequestDeserializer());
-		builder.registerTypeAdapter(MLResult.class, new MLResultDeserializer());
-		builder.registerTypeAdapter(MLValue.class, new MLValueSerializer());
-		gson = builder.create();
-	}
+    /**
+     * Parses a {@link MLRequest} from an {@link InputStream}.
+     *
+     * @param is the <code>InputStream</code> to parse from
+     *
+     * @return the parsed <code>MLRequest</code>
+     */
+    public MLRequest parseRequest(InputStream is) {
+        return gson.fromJson(new InputStreamReader(is), MLRequest.class);
+    }
 
-	/**
-	 * Parses a {@link MLRequest} from an {@link InputStream}.
-	 * 
-	 * @param is the <code>InputStream</code> to parse from
-	 * @return the parsed <code>MLRequest</code>
-	 */
-	public MLRequest parseRequest(InputStream is) {
-		return gson.fromJson(new InputStreamReader(is), MLRequest.class);
-	}
+    /**
+     * Parses a {@link MLRequest} from a {@link String}.
+     *
+     * @param json the <code>String</code> to parse from
+     *
+     * @return the parsed <code>MLRequest</code>
+     */
+    public MLRequest parseRequest(String json) {
+        return gson.fromJson(json, MLRequest.class);
+    }
 
-	/**
-	 * Outputs a {@link MLRequest} to an {@link OutputStream}.
-	 * 
-	 * @param request the <code>MLRequest</code> to output
-	 * @param os the <code>OutputStream</code> to output to
-	 */
-	public void outputRequest(MLRequest request, OutputStream os){		
-		String json = gson.toJson(request);
-		PrintWriter pw = new PrintWriter(os, true);
-		pw.println(json);
-	}
+    /**
+     * Parses a {@link MLException} from an {@link InputStream}.
+     *
+     * @param is the <code>InputStream</code> to parse from
+     *
+     * @return the parsed <code>MLException</code>
+     */
+    public MLException parseException(InputStream is) {
+        return gson.fromJson(new InputStreamReader(is), MLException.class);
+    }
 
-	/**
-	 * Outputs a {@link MLResult} to an {@link OutputStream}.
-	 * 
-	 * @param result the <code>MLResult</code> to output
-	 * @param os the <code>OutputStream</code> to output to
-	 */
-	public void outputResult(MLResult result, OutputStream os) {
-		String json = gson.toJson(result);
-		PrintWriter pw = new PrintWriter(os, true);
-		pw.println(json);
-	}
+    /**
+     * Parses a {@link MLException} from a {@link String}.
+     *
+     * @param json the <code>String</code> to parse from
+     *
+     * @return the parsed <code>MLException</code>
+     */
+    public MLException parseException(String json) {
+        return gson.fromJson(json, MLException.class);
+    }
 
-	/**
-	 * Outputs a {@link MLException} to an {@link OutputStream}.
-	 * 
-	 * @param exception the <code>MLException</code> to output
-	 * @param os the <code>OutputStream</code> to output to
-	 */
-	public void outputException(MLException exception, OutputStream os) {
-		String json = gson.toJson(exception);
-		PrintWriter pw = new PrintWriter(os, true);
-		pw.println(json);
-	}
+    /**
+     * Parses a {@link MLResult} from an {@link InputStream}.
+     *
+     * @param is the <code>InputStream</code> to parse from
+     *
+     * @return the parsed <code>MLResult</code>
+     */
+    public MLResult parseResult(InputStream is) {
+        return gson.fromJson(new InputStreamReader(is), MLResult.class);
+    }
 
-	/**
-	 * Sends a request to a MATLAB server. The server must be using the supplied server.m and waiting for 
-	 * connections on the specified port.
-	 * 
-	 * @param host the address of the MATLAB server
-	 * @param port the port of the MATLAB server
-	 * @param request the <code>MLRequest</code> to send
-	 * @return the <code>MLResult</code> of the function
-	 * @throws MLException if MATLAB encountered an error during function execution
-	 * @throws IOException if the connection to the MATLAB server failed
-	 */
-	// TODO: sometimes a SocketException gets thrown (possible to do with lots of requests in a short amount of time), for now there's just three attempts if this happens
-	public MLResult sendRequest(String host, int port, MLRequest request) throws MLException, IOException {
-		int attempt = 0;
-		SocketException thrown = null;
-		while (attempt < 3) {
-			try {
-				// create socket
-				SocketAddress address = new InetSocketAddress(host, port);
-				Socket socket = new Socket();
-				socket.connect(address, 10 * 1000); // 10s to connect
-				
-				// send request
-				OutputStream out = socket.getOutputStream();
-				InputStream in = socket.getInputStream();
-				outputRequest(request, out);
+    /**
+     * Parses a {@link MLResult} from a {@link String}.
+     *
+     * @param json the <code>String</code> to parse from
+     *
+     * @return the parsed <code>MLResult</code>
+     */
+    public MLResult parseResult(String json) {
+        return gson.fromJson(json, MLResult.class);
+    }
 
-				// get response
-				char[] buffer = new char[1024];
-				BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-				StringWriter writer = new StringWriter();
-				int c;
-				while ((c = reader.read(buffer)) != -1) {
-					writer.write(buffer, 0, c);
-				}
-				String response = writer.toString();
+    /**
+     * Outputs a {@link MLRequest} to an {@link OutputStream}.
+     *
+     * @param request the <code>MLRequest</code> to output
+     * @param os      the <code>OutputStream</code> to output to
+     */
+    public void outputRequest(MLRequest request, OutputStream os) {
+        print(request, os);
+    }
 
-				// parse
-				// try for exception
-				// speedup, not the best
-				if (response.startsWith("{\"exception\"")) {
-					MLException exception = gson.fromJson(response, MLException.class);
+    /**
+     * Outputs a {@link MLResult} to an {@link OutputStream}.
+     *
+     * @param result the <code>MLResult</code> to output
+     * @param os     the <code>OutputStream</code> to output to
+     */
+    public void outputResult(MLResult result, OutputStream os) {
+        print(result, os);
+    }
 
-					out.close();
-					socket.close();
+    /**
+     * Outputs a {@link MLException} to an {@link OutputStream}.
+     *
+     * @param exception the <code>MLException</code> to output
+     * @param os        the <code>OutputStream</code> to output to
+     */
+    public void outputException(MLException exception, OutputStream os) {
+        print(exception, os);
+    }
 
-					throw exception;
-				}
-				else {
-					// not an error, must be result
-					MLResult result = gson.fromJson(response, MLResult.class);			
+    public <T> T parse(String json, Class<T> type) {
+        return gson.fromJson(json, type);
+    }
 
-					out.close();
-					socket.close();
+    public <T> T parse(InputStream is, Class<T> type) {
+        return gson.fromJson(new InputStreamReader(is), type);
+    }
 
-					return result;
-				}
-			}
-			catch (SocketException e) {
-				thrown = e;
-				attempt++;
-			}
-		}
-		
-		throw thrown;
-	}
-
-	public MLResult sendRequest(String mlProxyURL, MLRequest request) throws MLException, IOException {
-		// open connection to proxy
-		URLConnection connection = new URL(mlProxyURL).openConnection();
-
-		// do a http post request
-		connection.setDoOutput(true);
-		OutputStream out = connection.getOutputStream();
-		out.write("request=".getBytes());
-		outputRequest(request, out);
-		out.close();
-
-		// get response
-		InputStream in = connection.getInputStream();
-		char[] buffer = new char[1024];
-		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-		StringWriter writer = new StringWriter();
-		int c;
-		while ((c = reader.read(buffer)) != -1) {
-			writer.write(buffer, 0, c);
-		}
-		String response = writer.toString();
-
-		// parse
-		try {
-			// try for exceptions
-			try {
-				MLException exception = gson.fromJson(response, MLException.class);
-				throw exception;
-			}
-			catch (JsonParseException e) {
-				IOException exception = gson.fromJson(response, IOException.class);
-				throw exception;
-			}
-		}
-		catch (JsonParseException e) {
-			// not an error, must be result
-			MLResult result = gson.fromJson(response, MLResult.class);			
-			return result;
-		}
-		finally {
-			in.close();
-		}
-	}
-
+    private void print(Object o, OutputStream os) {
+        String json = gson.toJson(o);
+        PrintWriter pw = new PrintWriter(os, true);
+        pw.println(json);
+    }
 }
